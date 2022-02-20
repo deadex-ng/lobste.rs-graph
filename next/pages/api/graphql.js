2827ffef146b019b686bfce7eecbe243b87c2875
deadex-ng <thisisfumba@gmail.com>
2022-02-20 08:08:33 +0200

    load Apollo studio

--- next/pages/api/graphql.js
+++ next/pages/api/graphql.js
@@ -13,7 +13,7 @@ const typeDefs = gql`
     comments: String
     created: DateTime
     user: User @relationship(type: "SUBMITTED", direction: IN)
-    tags: [Tag] @relationship(type: "HAS_TAG", direction: OUT)
+    tags: [Tag!]! @relationship(type: "HAS_TAG", direction: OUT)
   }
   type User @exclude(operations: [CREATE, UPDATE, DELETE]) {
     username: String
@@ -22,11 +22,11 @@ const typeDefs = gql`
     about: String
     avatar: String
     invited_by: User @relationship(type: "INVITED_BY", direction: OUT)
-    articles: [Article] @relationship(type: "SUBMITTED", direction: OUT)
+    articles: [Article!]! @relationship(type: "SUBMITTED", direction: OUT)
   }
   type Tag @exclude(operations: [CREATE, UPDATE, DELETE]) {
     name: String
-    articles: [Article] @relationship(type: "HAS_TAG", direction: IN)
+    articles: [Article!]! @relationship(type: "HAS_TAG", direction: IN)
   }
 `;
 
@@ -35,25 +35,23 @@ const driver = neo4j.driver(
     neo4j.auth.basic(process.env.NEO4J_USER, process.env.NEO4J_PASSWORD)
 );
 
-
-const neoSchema = new Neo4jGraphQL({typeDefs, driver});
-console.log(`VALUE OF NEOSCHEMA ${driver}`);
-const apolloServer = new ApolloServer({
-  //schema: new Neo4jGraphQL({typeDefs, driver}),
-  schema: neoSchema.schema,
-  playground: true,
-  introspection: true,
-  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
-});
-
-const startServer = apolloServer.start();
-
 export default async function handler(req, res) {
-  await startServer;
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "https://studio.apollographql.com");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  if (req.method === "OPTIONS") {
+      res.end();
+      return false;
+  }
+
+  const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+  const apolloServer = new ApolloServer({ schema: await neoSchema.getSchema() });
+  await apolloServer.start();
   await apolloServer.createHandler({
-    path: "/api/graphql",
+      path: "/api/graphql",
   })(req, res);
 }
+
 
 export const config = {
   api: {
